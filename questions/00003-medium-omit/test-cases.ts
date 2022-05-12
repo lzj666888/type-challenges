911fc02604e063e47326df7947afd121d17ff2af
lzj <lizongjie.163@bytedance.com>
2022-05-12 10:11:12 +0800

    feat: 3-m-myOmit

--- questions/00003-medium-omit/test-cases.ts
+++ questions/00003-medium-omit/test-cases.ts
@@ -1,5 +1,8 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+// 从 T 对象类型中Pick出属性：属性是T的keys 但是key又不能在P中出现
+type MyOmit<T, P> = Pick<T, Exclude<keyof T, P>>
+
 type cases = [
   Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
   Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>,
