de8963f4647ab1f513f21713dc287a06d615e6f2
lzj <lizongjie.163@bytedance.com>
2022-05-12 10:23:32 +0800

    feat: 3-e-myPick

--- questions/00004-easy-pick/test-cases.ts
+++ questions/00004-easy-pick/test-cases.ts
@@ -1,4 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
 
 type cases = [
   Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
